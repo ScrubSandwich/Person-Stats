de5e5e6257ae4b20e6d95e64498bf0557e1415e7
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-06-26 13:06:42 -0500

    Completed homework 1

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-function updateHeading(ev) {
+function updatePage(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
-    const heading = document.querySelector('h1')
+    const color = f.favoriteColor.value
 
-    // heading.innerHTML = name
-    heading.textContent = name
+    //Add the name and the color to a Paragraph in the details div
+    document.querySelector("#name").textContent = "Your Name: " + name
+    document.querySelector("#color").textContent = "Your Favorite Color: " + color
+    
+    //Add some color
+    document.body.style.color = color
 }
 
 const personForm = document.querySelector('#person-form')
-personForm.addEventListener('submit', updateHeading)
+personForm.addEventListener('submit', updatePage)
