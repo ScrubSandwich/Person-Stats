c2ab89bc8767a09ef72664471e99725d64b6f416
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-06-26 11:08:02 -0500

    Update heading with input text

---
+++ index.js
@@ -0,0 +1,12 @@
+function updateHeading(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const name = f.personName.value
+    const heading = document.querySelector('h1')
+
+    // heading.innerHTML = name
+    heading.textContent = name
+}
+
+const personForm = document.querySelector('#person-form')
+personForm.addEventListener('submit', updateHeading)
