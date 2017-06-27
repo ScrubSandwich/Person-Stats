dbb3720187b5d976e43280b5ef25eab2c3ea49f7
Jacob Miecznikowski <jmieczni@purdue.edu>
2017-06-27 10:02:06 -0400

    Finishing touches on the project

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function updatePage(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const color = f.favoriteColor.value
-
-    //Add the name and the color to a Paragraph in the details div
-    document.querySelector("#name").textContent = "Your Name: " + name
-    document.querySelector("#color").textContent = "Your Favorite Color: " + color
-    
-    //Add some color
-    document.body.style.color = color
+	ev.preventDefault()
+	const f = ev.target
+	const name = f.personName.value
+	const age = f.age.value
+	const color = f.favoriteColor.value
+	
+	//Add the name and the color to a Paragraph in the details div
+	document.querySelector("#name").textContent = "Your Name: " + name
+	document.querySelector("#age").textContent = "Your Age: " + age
+	document.querySelector("#color").textContent = "Your Favorite Color: " + color
+	
+	//Add some color
+	document.body.style.color = color
 }
 
 const personForm = document.querySelector('#person-form')
